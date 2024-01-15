93fe7a2212e7a27639cbd4e10379d92b2fa21078
FrankGrillo1991 <fsg12491@aol.com>
2024-01-15 18:36:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const ham = document.querySelector(".hamburger");
 const header = document.querySelector(".header");
 const featuresBtn = document.querySelector(".features-btn");
 const hiidenFeaturesBtn = document.querySelector(".hidden-features");
+const companyBtn = document.querySelector(".company-btn");
 const hiddenCompanyBtn = document.querySelector(".hidden-company");
 
 ham.addEventListener("click", function(){
     header.classList.toggle("active");
 })
+
+featuresBtn.addEventListener("click", function() {
+    hiddenFeaturesBtn.classList.toggle("active");
+})
+
+companyBtn.addEventListener("click", function() {
+    hiddenCompanyBtn.classList.toggle("active");
+})
+
+
+
+
