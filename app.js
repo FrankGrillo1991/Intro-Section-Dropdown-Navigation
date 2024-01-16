46422a86c1c46ec1eb812d69383687d287d744bf
FrankGrillo1991 <fsg12491@aol.com>
2024-01-16 05:05:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const menu = document.querySelector(".menu");
 const dropDown = document.querySelectorAll(".dropdown");
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", function() {
     menu.parentElement.classList.toggle("open");
     document.body.classList.toggle("nav-open");
 });
 
 dropDown.forEach(item) {
-    item.addEventListener("click", () => {
+    item.addEventListener("click", function() {
         item.parentElement.classList.toggle("link-open");
     });
 });
