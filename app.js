131393f4e3fd9d2b7ee08c8428d17049a20fcf76
FrankGrillo1991 <fsg12491@aol.com>
2024-01-17 06:42:20 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const navigationList = document.querySelector(".navigation__list");
+const navigation = document.querySelector(".navigation");
+const btnMobile = document.querySelector(".btn-mobile");
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+
+const showDropDown = (el) => {
+    const clicked = el.target.closest(".navigation__item__dropdown")
+
+    clicked.classList.toggle("active");
+};
+
+const showMobileNav = ()  => {
+    navigation.classList.toggle("active")
+    html.classList.toggle("active");
+    html.classList.toggle("u-overflow-y-hidden");
+    body.classList.toggle("u-overflow-y-hidden");
+};
+
+navigationList.addEventListener("click", showDropDown);
+
+btnMobile.addEventListener("click", showMobileNav);
